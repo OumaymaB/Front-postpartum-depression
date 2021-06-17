2edc7d931a53c7a512ac496dc5be15ac7024094d
oumayma boudarssa <oumayma98seoul@gmail.com>
2021-06-17 15:23:26 +0100

    update modifier compte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'edit-password',
     loadChildren: () => import('./edit-password/edit-password.module').then( m => m.EditPasswordPageModule)
   },
