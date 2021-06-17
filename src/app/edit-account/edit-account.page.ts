7a79e3717bb03c0b52e8e6452280956213834708
oumayma boudarssa <oumayma98seoul@gmail.com>
2021-06-17 21:20:20 +0100

    modifier compte done

--- src/app/edit-account/edit-account.page.ts
+++ src/app/edit-account/edit-account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-edit-account',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAccountPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  passwordLink(){
+   this.router.navigate(['/edit-password']);
   }
 
 }
