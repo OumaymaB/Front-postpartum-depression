1c3aefa013e8243b8ceba7ba5d96db427f7b2408
Hajar-Oe <h.ousguine_etu@enset-media.ac.ma>
2021-06-19 03:04:28 +0100

    Register Added

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,83 @@
+import { Component,  Input,  ViewChild } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonInput } from '@ionic/angular';
+import { PasswordValidator } from '../validators/passwd';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage{
+  @ViewChild('signupSlider') signupSlider;
+
+  public slideOneForm: FormGroup;
+	public slideTwoForm: FormGroup;
+  public slideThreeForm: FormGroup;
+
+	public submitAttempt: boolean = false;
+  constructor(public formBuilder: FormBuilder) { 
+    this.slideOneForm = formBuilder.group({
+      prenom: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      nom: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      dateNaissance: ['']
+  });
+
+  this.slideTwoForm = formBuilder.group({
+    nomuser: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    email: ['',
+    Validators.compose([
+    Validators.required,
+		Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])],
+   	motdepasse: ['', Validators.compose([
+	 	Validators.minLength(5),
+	 	Validators.required,
+	 	Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+    ])],
+    motdepasseconfirm: ['',Validators.required]
+}, {validator: PasswordValidator.passwordsMatch});
+
+    this.slideThreeForm = formBuilder.group({
+      bio: [''],
+      photo: ['']
+      });
+    }
+
+  
+  next(){
+    this.signupSlider.slideNext();
+  }
+
+    prev(){
+        this.signupSlider.slidePrev();
+    }
+
+    save(){
+
+      this.submitAttempt = true;
+
+      if(!this.slideOneForm.valid){
+          this.signupSlider.slideTo(0);
+      } 
+      else if(!this.slideTwoForm.valid){
+          this.signupSlider.slideTo(1);
+      }
+      else if(!this.slideThreeForm.valid){
+        this.signupSlider.slideTo();
+    }
+      else {
+          console.log("success!")
+          console.log(this.slideOneForm.value);
+          console.log(this.slideTwoForm.value);
+          console.log(this.slideThreeForm.value);
+      }
+  }
+  /*ngAfterViewInit() {
+    setTimeout(() => {
+      this.textinput.setFocus();
+      console.log("After the view");
+  }, 400);
+}*/
+}
+
+
