92dbac4a8bc28f0567d954d5a5dfa6f228b41aa2
Zineb <zinebboukhari192@gmail.com>
2021-06-19 12:43:42 +0100

    interfaces

---
+++ src/app/reset-code/reset-code.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-code',
+  templateUrl: './reset-code.page.html',
+  styleUrls: ['./reset-code.page.scss'],
+})
+export class ResetCodePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
