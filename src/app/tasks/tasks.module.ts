634da0545980b86b04eb47511713dc689285ec3c
Hajar-Oe <h.ousguine_etu@enset-media.ac.ma>
2021-06-20 16:42:06 +0100

    task module change

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -7,13 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { TasksPageRoutingModule } from './tasks-routing.module';
 
 import { TasksPage } from './tasks.page';
+import { NgCalendarModule } from 'ionic2-calendar';
+import { TaskModalPage } from './task-modal/task-modal.page';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TasksPageRoutingModule
+    TasksPageRoutingModule,
+    TaskModalPage
   ],
   declarations: [TasksPage]
 })
