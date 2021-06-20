53387a0fda7cd7b1a9981379a6d62c41d903d033
oumayma boudarssa <oumayma98seoul@gmail.com>
2021-06-20 10:53:37 +0100

    added publication page with routers

--- src/app/add-publication/add-publication.page.ts
+++ src/app/add-publication/add-publication.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-publication',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPublicationPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  annuler(){
+    this.router.navigate(['accueil']);
   }
 
 }
