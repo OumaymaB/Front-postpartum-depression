f964cbf168aaf7cc1da15becdf5030a7805cf125
oumayma boudarssa <oumayma98seoul@gmail.com>
2021-06-20 12:08:41 +0100

    add alert on button delete - profil

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: 'Etes-vous certaine de vouloir supprimer la publication?',
+      buttons: ['Non', 'Oui']
+    });
+
+    await alert.present();
   }
 
 }
