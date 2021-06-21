023fb796f065317c5191279cfd9c4082a3739bb8
Hajar-Oe <h.ousguine_etu@enset-media.ac.ma>
2021-06-21 22:02:22 +0100

    form validation done

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,7 @@
 import { Component,  Input,  ViewChild } from '@angular/core';
 import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { IonInput } from '@ionic/angular';
-import { PasswordValidator } from '../validators/passwd';
+import { MustMatch } from '../validators/passwd';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -10,67 +10,46 @@ import { PasswordValidator } from '../validators/passwd';
 export class RegisterPage{
   @ViewChild('signupSlider') signupSlider;
 
-  public slideOneForm: FormGroup;
-	public slideTwoForm: FormGroup;
-  public slideThreeForm: FormGroup;
+  public Form: FormGroup;
+	/*public slideTwoForm: FormGroup;
+  public slideThreeForm: FormGroup;*/
   selectedFile=null;
 
 	public submitAttempt: boolean = false;
   constructor(public formBuilder: FormBuilder) { 
-    this.slideOneForm = formBuilder.group({
+    this.Form = formBuilder.group({
       prenom: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
       nom: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-      dateNaissance: ['']
-  });
-
-  this.slideTwoForm = formBuilder.group({
-    nomuser: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-    email: ['',
-    Validators.compose([
-    Validators.required,
-		Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-    ])],
-   	motdepasse: ['', Validators.compose([
-	 	Validators.minLength(5),
-	 	Validators.required,
-	 	Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
-    ])],
-    motdepasseconfirm: ['',Validators.required]
-}, {validator: PasswordValidator.passwordsMatch});
-
-    this.slideThreeForm = formBuilder.group({
+      dateNaissance: [''],
+      nomuser: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ['',
+      Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])],
+       motdepasse: ['', Validators.compose([
+       Validators.minLength(5),
+       Validators.required,
+       Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])],
+      motdepasseconfirm: ['',Validators.required],
       bio: [''],
-      photo: [this.selectedFile]
-      });
-    }
-
-  
-  next(){
-    this.signupSlider.slideNext();
-  }
-
-    prev(){
-        this.signupSlider.slidePrev();
-    }
+      photo: [this.selectedFile]      
+  },{
+    validator: MustMatch('motdepasse','motdepasseconfirm')
+  });
+    } 
 
     save(){
 
       this.submitAttempt = true;
 
-      if(!this.slideOneForm.valid){
+      if(!this.Form.valid){
           this.signupSlider.slideTo(0);
       } 
-      else if(!this.slideTwoForm.valid){
-          this.signupSlider.slideTo(1);
-      }
-      else if(!this.slideThreeForm.valid){
-        this.signupSlider.slideTo();
-    }
       else {
           console.log("success!")
-          console.log(this.slideOneForm.value);
-          console.log(this.slideTwoForm.value);
-          console.log(this.slideThreeForm.value);
+          console.log(this.Form.value);
       }
   }
  
@@ -78,7 +57,13 @@ export class RegisterPage{
    this.selectedFile=$event.target.files[0];
     console.log(this.selectedFile);
   }
-  
+  next(){
+    this.signupSlider.slideNext();
+  }
+
+    prev(){
+        this.signupSlider.slidePrev();
+    }
 }
 
 
--- src/app/validators/passwd.ts
+++ src/app/validators/passwd.ts
@@ -1,38 +1,24 @@
 import { FormControl, FormGroup } from '@angular/forms';
 
-export class PasswordValidator {
-// Inspired on: http://plnkr.co/edit/Zcbg2T3tOxYmhxs7vaAm?p=preview
-/*static areEqual(formGroup: FormGroup) {
-	let val;
-	let valid = true;
-
-	for (let key in formGroup.controls) {
-		if (formGroup.controls.hasOwnProperty(key)) {
-			let control: FormControl = <FormControl>formGroup.controls[key];
-			if (val === undefined) {
-				val = control.value
-			} else {
-				if (val !== control.value) {
-					valid = false;
-					break;
-				}
-			}
-		}
-	}
-	if (valid) {
-		return null;
-	}
-	return {
-		areEqual: true
-	}
- }*/
- static passwordsMatch(cg: FormGroup): {[err: string]: any} {
-    let pwd1 = cg.get('motdepasse');
+export function MustMatch(controlName: string, matchingControlName: string){
+    /*let pwd1 = cg.get('motdepasse');
     let pwd2 = cg.get('motdepasseconfirm');
     let rv: {[error: string]: any} = {};
     if ((pwd1.touched || pwd2.touched) && pwd1.value !== pwd2.value) {
       rv['passwordMismatch'] = true;
     }
-    return rv;
+    return rv;*/
+	return(formGroup: FormGroup)=>{
+		const control= formGroup.controls[controlName];
+		const matchingcontrol= formGroup.controls[matchingControlName];
+		if(matchingcontrol.errors && !matchingcontrol.errors.MustMatch){
+			return;
+		}
+		if(control.value !== matchingcontrol.value){
+			matchingcontrol.setErrors({MustMatch: true});
+		}else{
+			matchingcontrol.setErrors(null);
+		}
+
+	}
   }
-}
