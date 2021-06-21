669d4439fbbff633b57c3abe56e911e06e72a82b
oumayma boudarssa <oumayma98seoul@gmail.com>
2021-06-22 00:37:50 +0100

    service login done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import { UserService } from '../service/user.service';
 
 
 @Component({
@@ -8,12 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-
+  form: FormGroup;
+  user : any = {};
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    public userService : UserService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+  }
 
+  submit(): void {
+ const val = this.form.value;
+ this.userService.login(val.email,val.password).subscribe(
+ res =>{
+   this.user = res;
+   localStorage.setItem("email", this.user.email);
+   let jwt = "innovation "+this.user.jwt;
+ localStorage.setItem("token",jwt)
+ this.userService.islogin = true;
+this.router.navigate(['/accueil']);
 
+ },
+ error => 
+ console.log(this.user)
+ );
+ 
+  }
 }
